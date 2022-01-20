aa5f21ad038a3541f56e6b598b23f51b0c717619
Lahiru Jayawardhana <71606225+repo-son@users.noreply.github.com>
2022-01-20 12:18:16 +0530

    File Structure Changed, Old Controller Deleted

---
+++ TestWebAPI/TestWebAPI/TestWebAPI/Data/TodoDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestWebAPI.Model;
+
+namespace TestWebAPI.Data
+{
+    public class TodoDB : DbContext
+    {
+        public TodoDB(DbContextOptions<TodoDB> options): base(options) 
+        {
+
+        }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
--- TestWebAPI/TestWebAPI/TestWebAPI/Program.cs
+++ TestWebAPI/TestWebAPI/TestWebAPI/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using TestWebAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDB>(opt => opt.UseInMemoryDatabase("TodoList"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
